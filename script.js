30f8960e9fd8c26a5b4dfc31f888fa6994b82572
Trinity Terry <trinitycterry@gmail.com>
2020-01-20 13:58:17 -0600

    validated text input

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let getInputText = () => {
 
 let cardNum = 0;
 let getCardHTML = (cardText, cardNum) => {
-    
+
     return `
         <p>${cardText}</p>
         <button id="${cardNum}" class="cardButton">delete</button>
@@ -16,11 +16,14 @@ let getCardHTML = (cardText, cardNum) => {
 let createCard = () => {
     cardNum++;
     let text = getInputText();
-    let cardContainer = document.querySelector(".card-container");
-    let card = document.createElement("div");
-    card.id = `card-${cardNum}`;
-    card.innerHTML = getCardHTML(text, cardNum);  
-    cardContainer.appendChild(card);
+    if (text !== "") {
+        let cardContainer = document.querySelector(".card-container");
+        let card = document.createElement("div");
+        card.id = `card-${cardNum}`;
+        card.innerHTML = getCardHTML(text, cardNum);
+        cardContainer.appendChild(card);
+    }
+
 };
 
 let deleteCard = (num) => {
@@ -28,13 +31,25 @@ let deleteCard = (num) => {
     document.getElementById(`card-${num}`).remove();
 };
 
+let clearTextBox = () => {
+    let textInput = document.getElementByName("cardInputForm");
+    document.forms.cardInputForm.reset();
+}
 
-document.getElementById("createBtn").addEventListener("click", () => createCard());
+document.getElementById("createBtn").addEventListener("click", (e) => {
+    let input = document.getElementById("textInput").value;
+    if (!(/\S/.test(input))) {
+        e.preventDefault();
+    } else if (input !== "") {
+        createCard();
+        document.forms.cardInputForm.reset();
+        e.preventDefault();
+    }
 
-document.addEventListener('click',function(e){
-    if(e.target && e.target.className=='cardButton'){
-          deleteCard(e.target.id);
+});
+
+document.addEventListener('click', function (e) {
+    if (e.target && e.target.className == 'cardButton') {
+        deleteCard(e.target.id);
     }
 });
-  
-
